c224437f3427e93acbf12b714727a5578307682e
Sean Coughlin <seanmc4@illinois.edu>
2021-12-06 19:58:04 -0600

    Login page framework

--- recipefork-client/src/App.tsx
+++ recipefork-client/src/App.tsx
@@ -1,25 +1,64 @@
 import React from 'react';
 import logo from "./resource/recipeFork.png"
+import k from "./resource/K.png"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <img src={logo}/>
+      <h1>RecipeFork</h1>
       <form>
-        <label>emails</label>
+        <label>Email</label>
         <input
           type="text" />
-        <label>password</label>
+        <label>Password</label>
         <input
           type="password" />
       </form>
       <a>Forgot password</a>
       <button>Login</button>
       Not a member? <a>Sign up</a>
+
+      <img src={k}/>
+      <h1>RecipeFork</h1>
+      <h2>Create Account</h2>
+      <form>
+        <label>Username</label>
+        <input
+          type="text" />
+        <label>Email</label>
+        <input
+          type="text" />
+        <label>Password</label>
+        <input
+          type="password" />
+        <label>Verify Password</label>
+        <input
+          type="password" />
+      </form>
+
+      <h1>RecipeFork</h1>
+      <h2>Reset Password</h2>
+      Forgot password?
+      <form>
+      <label>Email</label>
+      <input
+        type="text" />
+      </form>
+      <button>Send Password Reset Email</button>
+
+      Recipes
+      Activity
+
+      Username
+      Posted Recipes
+      Forks
+      Pinned Recipes
+      Contributions
+
       <header className="App-header">
-        Recipes
-        Activity
+        
       </header>
     </div>
   );
