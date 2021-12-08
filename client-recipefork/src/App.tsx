401d5cc9215577afe06aed861b44b1df5b81a141
Sean Coughlin <seanmc4@illinois.edu>
2021-12-08 13:43:56 -0600

    Add browserrouter and bootstrap

--- client-recipefork/src/App.tsx
+++ client-recipefork/src/App.tsx
@@ -8,6 +8,11 @@ import ResetPassword from './resetPassword';
 import HomePage from "./homePage"
 import UserPage from './userPage';
 import RecipePage from './recipePage';
+import {
+  Route,
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
@@ -15,8 +20,15 @@ function App() {
       <img src={logo} />
       <h1>RecipeFork</h1>
 
-      <HomePage/>
-      <UserPage/>
+      <div>
+        <Route path="/recipeFork/" exact component={Login} />
+        <Route path="/recipeFork/login" exact component={Login} />
+        <Route path="/recipeFork/createAccount" exact component={CreateAccnt} />
+        <Route path="/recipeFork/forgotPassword" exact component={ResetPassword} />
+        <Route path="/recipeFork/home" exact component={HomePage} />
+        <Route path="/recipeFork/userPage" exact component={UserPage} />
+        <Route path="/recipeFork/recipePage" exact component={RecipePage} />
+      </div>
     </div>
   );
 }
@@ -29,11 +41,4 @@ export default App;
 
 /*
       <img src={k} />
-<div className="item">
-        <Route path="/recipeFork/" exact component={CreateAccnt} />
-        <Route path="/recipeFork/login" exact component={Login} />
-        <Route path="/recipeFork/forgotPassword" exact component={ResetPassword} />
-        <Route path="/recipeFork/home" exact component={HomePage} />
-        <Route path="/recipeFork/userPage" exact component={UserPage} />
-        <Route path="/recipeFork/recipePage" exact component={RecipePage} />
-      </div>*/
+*/
--- client-recipefork/src/index.tsx
+++ client-recipefork/src/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-/*
-<BrowserRouter>
-    <App />
-  </BrowserRouter>,*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
