60c657d84d24aa29a97c53e0f3baeb2f049b75cd
Sean Coughlin <seanmc4@illinois.edu>
2021-12-10 10:28:32 -0600

    Update navbar

--- client-recipefork/src/navbar.tsx
+++ client-recipefork/src/navbar.tsx
@@ -3,32 +3,29 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import k from "./resource/K.png"
-
-/*
-sign out
 import { getAuth, signOut } from "firebase/auth";
 
-const auth = getAuth();
-signOut(auth).then(() => {
-  // Sign-out successful.
-}).catch((error) => {
-  // An error happened.
-});*/
-
 class AppNavbar extends React.Component {
+    logout = () => {
+        const auth = getAuth();
+        signOut(auth).then(() => {
+          // Sign-out successful.
+        }).catch((error) => {
+          // An error happened.
+        })
+    }
+
     render() {
         return <div className="app">
             <Navbar fixed="top" bg="light">
-                <Navbar.Brand href="#home">RecipeFork</Navbar.Brand>
+                <Navbar.Brand href="/recipeFork/home">RecipeFork</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <NavDropdown title={<img className="profile-thumb" src={k}/>} id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <NavDropdown.Item href="/recipeFork/userPage">Profile</NavDropdown.Item>
+                            <NavDropdown.Item href="/recipeFork/createRecipePage">New Recipe</NavDropdown.Item>
+                            <NavDropdown.Item onClick={this.logout}>Sign Out</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
