55023c33c6c5bd60ca466147631742c1808218c4
Kyle McNamara <kylegm3@illinois.edu>
2021-12-12 15:40:31 -0600

    made recipe schema

--- server-recipefork/models/recipe.ts
+++ server-recipefork/models/recipe.ts
@@ -1,3 +1,19 @@
-// import mongoose
+// Load required packages
+var mongoose = require('mongoose');
 
-// TODO: set up mongoose schema for users
+// Define our recipe schema
+var RecipeSchema = new mongoose.Schema({
+    name: String,
+    userId: String,
+    prepTime: String,
+    cookTime: String,
+    ingredients: [String],
+    instructions: [String],
+    tags: [String],
+    image: [String],
+    forks: Number,
+    forkOrigin: String
+});
+
+// Export the Mongoose model
+module.exports = mongoose.model('Recipe', RecipeSchema);
