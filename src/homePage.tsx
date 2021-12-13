8df5e0238b64bd1ab9d764fe0c0bb1a09abcc645
Sean Coughlin <seanmc4@illinois.edu>
2021-12-13 11:37:25 -0600

    Add axios

--- src/homePage.tsx
+++ src/homePage.tsx
@@ -5,13 +5,29 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { UserCredential } from '@firebase/auth';
+import axios from 'axios';
 
 interface AuthState {
     auth: UserCredential;
 }
 
+
 class HomePage extends React.Component<AuthState> {
+    state = {
+        recipes: []
+    }
+
+    componentDidMount() {
+        axios.get(`http://localhost:4000/api/recipes?where={"_id": {"$in": ["61b718bfb4e3958cfa8dc9be","61b718d0b4e3958cfa8dc9c0"]}}&sort={"name": 1}&select={"_id": 0}`)
+            .then(res => {
+                const recipes = res.data.data;
+                console.log(typeof (res.data.data), res.data.data, Object.values(res.data.data));
+                this.setState({ recipes });
+            })
+    }
+
     render() {
+        console.log("recipes", this.state.recipes)
         return <div className="app">
             <Container>
                 <Row>
@@ -21,6 +37,12 @@ class HomePage extends React.Component<AuthState> {
                     <Col>
                         <h4>Recipes</h4>
                         <ListGroup variant="flush">
+                            {
+                                this.state.recipes.map(
+                                    recipe =>
+                                        <ListGroup.Item><img className="profile-thumb" src={recipe["image"]} alt="recipe" />{recipe["name"]}</ListGroup.Item>
+                                )
+                            }
                             <ListGroup.Item action href="#link1"><img className="profile-thumb" src={k} alt="recipe" />Cras justo odio</ListGroup.Item>
                             <ListGroup.Item action href="#link1"><img className="profile-thumb" src={k} alt="recipe" />apibus ac facilisis in</ListGroup.Item>
                             <ListGroup.Item action href="#link1"><img className="profile-thumb" src={k} alt="recipe" />Morbi leo risus</ListGroup.Item>
