442d945448b1e65a52f229f3082973e4db87f2e0
Sean Coughlin <seanmc4@illinois.edu>
2021-12-15 09:06:30 -0600

    Initial searchbar

--- src/homePage.tsx
+++ src/homePage.tsx
@@ -6,6 +6,8 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { UserCredential } from '@firebase/auth';
 import axios from 'axios';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 interface AuthState {
     auth: UserCredential;
@@ -13,32 +15,101 @@ interface AuthState {
 
 class HomePage extends React.Component<AuthState> {
     state = {
-        recipes: []
+        results: [],
+        search: "",
+        recipe: true,
+        isSearching: false
     }
 
     openRecipe(id: string) {
         window.location.href = "/recipefork-frontend/recipePage?id=" + id;
     }
 
-    componentDidMount() {
-        axios.get(`https://recipefork-backend.herokuapp.com/api/recipes`)
-            .then(res => {
-                const recipes = res.data.data;
-                console.log(typeof (res.data.data), res.data.data, Object.values(res.data.data));
-                this.setState({ recipes });
-            })
+    search() {
+        this.setState({ isSearching: true })
+        if (this.state.recipe) {
+            axios.get(`https://recipefork-backend.herokuapp.com/api/recipes?where={"name":"${this.state.search}"}`)
+                .then(res => {
+                    const recipes = res.data.data;
+                    this.setState({ results: recipes });
+                }).catch(error => {
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                })
+        } else {
+            axios.get(`https://recipefork-backend.herokuapp.com/api/users?where={"username":"${this.state.search}"}`)
+                .then(res => {
+                    const users = res.data.data;
+                    this.setState({ results: users });
+                }).catch(error => {
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                })
+        }
     }
 
     render() {
-        console.log("recipes", this.state.recipes)
         return <div className="app">
             <Container>
                 <Row>
                     <h2 className="left-align">Howdy, {this.props.auth.user.email} !</h2>
                 </Row>
                 <Row>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="control1">
+                            <Form.Label>Search</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="Mac n Cheese"
+                                value={this.state.search}
+                                onChange={e => this.setState({ search: e.target.value })} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="control2">
+                            <Row>
+                                <Col className="center-align">
+                                    <Form.Check
+                                        inline
+                                        defaultChecked
+                                        label="Recipes"
+                                        name="group1"
+                                        type="radio"
+                                        id={`inline-radio-1`}
+                                        onChange={() => this.setState({ recipe: true })}
+                                    />
+                                    <Form.Check
+                                        inline
+                                        label="Users"
+                                        name="group1"
+                                        type="radio"
+                                        id={`inline-radio-2`}
+                                        onChange={() => this.setState({ recipe: false })}
+                                    />
+                                </Col>
+                                <Col className="center-align">
+                                    <Button onClick={() => this.search()}>Search</Button>
+                                </Col>
+                            </Row>
+                        </Form.Group>
+                    </Form>
+                    <Row>
+                        {this.state.isSearching && <div className="center-align">
+                            <h4>Search Results</h4>
+                            <ListGroup>
+                                {this.state.results.map((r: any) => {
+                                    return (
+                                        <ListGroup.Item key={r._id}>
+                                            {this.state.recipe ? <a href={"/recipefork-frontend/recipePage?id=" + r._id}>{r.name}</a> : <a href={"/recipefork-frontend/userPage?id=" + r._id}>{r.username}</a>}
+                                        </ListGroup.Item>)
+                                })}
+                            </ListGroup>
+                        </div>}
+                    </Row>
+                </Row>
+                <Row>
                     <h4>Recipes</h4>
-                    
                     {/*source: https://codepen.io/klesht/pen/pjjegK*/}
                     <div className="recipe-card">
                         <aside>
@@ -50,7 +121,7 @@ class HomePage extends React.Component<AuthState> {
 
                         <article>
                             <h2>Chai Oatmeal</h2>
-                        
+
                             <ul>
                                 <li><span className="icon icon-users"></span><span>1</span></li>
                                 <li><span className="icon icon-clock"></span><span>15 min</span></li>
@@ -65,15 +136,6 @@ class HomePage extends React.Component<AuthState> {
                         </article>
 
                     </div>
-            
-                    <h4>Activity</h4>
-
-                    <ul className="no-bullets">
-                        <li>asdf</li>
-                        <li><img className="profile-thumb" src={k} />asdf</li>
-                        <li><img className="profile-thumb" src={k} />asdf</li>
-                    </ul>
-                
                 </Row>
             </Container>
         </div>;
